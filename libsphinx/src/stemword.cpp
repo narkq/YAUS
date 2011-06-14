c39e058d918ec5c660dbda60b0f5381909137a66
narkq <sladkov@gmail.com>
2011-06-14 22:05:32 +0400

    added a couple of test for english stemmer, hmm..

--- libsphinx/src/stemword.cpp
+++ libsphinx/src/stemword.cpp
@@ -7,6 +7,7 @@ int main(int argc, char* argv[])
     BYTE pWord[100];
 
     stem_ru_init();
+    stem_en_init();
 
     if (argc > 1)
     {
@@ -16,6 +17,7 @@ int main(int argc, char* argv[])
     {
         scanf("%s\n", pWord);
     }
+    stem_en(pWord, strlen((char*) pWord));
     stem_ru_cp1251(pWord);
     printf("%s\n", pWord);
 
