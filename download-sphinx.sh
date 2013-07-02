04776a4c51b1e4c8739d4ef678f532fdf559e9bf
Alex Sladkov <sladkov@triumvirat.ru>
2013-07-02 15:26:13 +0400

    added download-sphinx script

---
+++ download-sphinx.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+SPHINX_VERSION=2.0.6
+wget http://sphinxsearch.com/files/sphinx-$SPHINX_VERSION-release.tar.gz \
+	&& tar zxf sphinx-$SPHINX_VERSION-release.tar.gz \
+	&& rm sphinx-$SPHINX_VERSION-release.tar.gz \
+	&& mv sphinx-$SPHINX_VERSION-release libsphinx
