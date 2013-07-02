1d84fa8723713f387f70112fbab336cf361ad0cf
Alex Sladkov <sladkov@triumvirat.ru>
2013-07-02 15:06:57 +0400

    added $is_utf boolean parameter for 'stemword_ru' and 'stemword_enru'

--- yaus.cpp
+++ yaus.cpp
@@ -44,14 +44,22 @@ PHP_FUNCTION(stemword_ru)
 	char *word;
 	char *result;
 	int wordlen;
-	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "s", &word, &wordlen) == FAILURE) {
+	int is_utf = 0;
+	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "s|b", &word, &wordlen, &is_utf) == FAILURE) {
 		RETURN_NULL();
 	}
 	//We need to get a copy of input parameters, because stem functions
 	//are destructive.
 	result = estrdup(word);
 
-	stem_ru_cp1251((BYTE *)result);
+	if (!is_utf)
+	{
+		stem_ru_cp1251((BYTE *)result);
+	}
+	else
+	{
+		stem_ru_utf8((WORD *)result);
+	}
 
 	Z_TYPE_P(return_value) = IS_STRING;
 	Z_STRVAL_P(return_value) = estrdup(result);
@@ -84,7 +92,8 @@ PHP_FUNCTION(stemword_enru)
 	char *word;
 	char *result;
 	int wordlen;
-	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "s", &word, &wordlen) == FAILURE) {
+	int is_utf = 0;
+	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "s|b", &word, &wordlen, &is_utf) == FAILURE) {
 		RETURN_NULL();
 	}
 	//We need to get a copy of input parameters, because stem functions
@@ -93,7 +102,14 @@ PHP_FUNCTION(stemword_enru)
 
 	//Theoretically, the order of stemming is irrelevant.
 	stem_en((BYTE *)result, wordlen);
-	stem_ru_cp1251((BYTE *)result);
+	if (!is_utf)
+	{
+		stem_ru_cp1251((BYTE *)result);
+	}
+	else
+	{
+		stem_ru_utf8((WORD *)result);
+	}
         
 	Z_TYPE_P(return_value) = IS_STRING;
 	Z_STRVAL_P(return_value) = estrdup(result);
