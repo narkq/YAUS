2141b7e70ba30ecb15fee2622e35a8e7973de58f
Alex Sladkov <sladkov@gmail.com>
2013-07-11 01:03:30 +0400

    -fPIC patch for ubuntu should be applied to libstemmer too

--- build-libsphinx.sh
+++ build-libsphinx.sh
@@ -4,12 +4,12 @@ wget http://sphinxsearch.com/files/sphinx-$SPHINX_VERSION-release.tar.gz \
 	&& tar zxf sphinx-$SPHINX_VERSION-release.tar.gz \
 	&& rm sphinx-$SPHINX_VERSION-release.tar.gz \
 	&& mv sphinx-$SPHINX_VERSION-release libsphinx \
-	&& patch -p0 < patches/sphinx-$SPHINX_VERSION-fPIC.patch \
 	&& pushd libsphinx \
-	&& ./buildconf.sh \
 	&& wget http://snowball.tartarus.org/dist/libstemmer_c.tgz \
 	&& tar zxf libstemmer_c.tgz \
 	&& rm libstemmer_c.tgz \
+	&& patch -p1 < patches/sphinx-$SPHINX_VERSION-fPIC.patch \
+	&& ./buildconf.sh \
 	&& ./configure --without-mysql --without-libexpat --without-iconv --with-libstemmer \
 	&& make -j2 \
 	&& popd
